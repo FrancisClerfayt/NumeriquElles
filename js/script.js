d0b58456dddd47ada80c7495d4dcb2f1d4d398c1
FrancisClerfayt <francis.clerfayt@gmail.com>
2020-11-03 21:27:30 +0100

    changed background-image so each page as a different one, added js folder and script.js file

---
+++ js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function(){
+	let body = document.querySelector('body');
+	let picture = document.getElementById('header_picture');
+	
+	let id = body.id;
+	
+	switch (id) {
+		case 'home':
+			picture.style.backgroundImage = 'url(../img/picture_header_one.jpg)';
+			break;
+		case 'going_further':
+			picture.style.backgroundImage = 'url(../img/picture_header_two.jpg)';
+			break;
+		case 'contact':
+			picture.style.backgroundImage = 'url(../img/picture_header_three.jpg)';
+			break;
+		case 'internship':
+			picture.style.backgroundImage = 'url(../img/picture_header_four.jpg)';
+			break;
+		case 'event_boulogne':
+			picture.style.backgroundImage = 'url(../img/picture_header_five.jpg)';
+			break;
+		case 'event_lens':
+			picture.style.backgroundImage = 'url(../img/picture_header_six.jpg)';
+			break;
+		case 'event_lille':
+			picture.style.backgroundImage = 'url(../img/picture_header_seven.jpg)';
+			break;
+		case 'event_maubeuge':
+			picture.style.backgroundImage = 'url(../img/picture_header_eight.jpg)';
+			break;
+		default:
+			picture.style.backgroundImage = 'url(../img/picture_header_one.jpg)';
+			break;
+	}
+});
