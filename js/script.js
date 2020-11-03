5237cc1fb1004f5d1e46ec4f75879c9a07d0a2ff
FrancisClerfayt <francis.clerfayt@gmail.com>
2020-11-03 22:53:53 +0100

    modified script.js for fixing background-image problems

--- js/script.js
+++ js/script.js
@@ -6,31 +6,31 @@ window.addEventListener('load', function(){
 	
 	switch (id) {
 		case 'home':
-			picture.style.backgroundImage = 'url(../img/picture_header_one.jpg)';
+			picture.style.backgroundImage = "url('../img/picture_header_one.jpg')";
 			break;
 		case 'going_further':
-			picture.style.backgroundImage = 'url(../img/picture_header_two.jpg)';
+			picture.style.backgroundImage = "url('../img/picture_header_two.jpg')";
 			break;
 		case 'contact':
-			picture.style.backgroundImage = 'url(../img/picture_header_three.jpg)';
+			picture.style.backgroundImage = "url('../img/picture_header_three.jpg')";
 			break;
 		case 'internship':
-			picture.style.backgroundImage = 'url(../img/picture_header_four.jpg)';
+			picture.style.backgroundImage = "url('../img/picture_header_four.jpg')";
 			break;
 		case 'event_boulogne':
-			picture.style.backgroundImage = 'url(../img/picture_header_five.jpg)';
+			picture.style.backgroundImage = "url('../img/picture_header_five.jpg')";
 			break;
 		case 'event_lens':
-			picture.style.backgroundImage = 'url(../img/picture_header_six.jpg)';
+			picture.style.backgroundImage = "url('../img/picture_header_six.jpg')";
 			break;
 		case 'event_lille':
-			picture.style.backgroundImage = 'url(../img/picture_header_seven.jpg)';
+			picture.style.backgroundImage = "url('../img/picture_header_seven.jpg')";
 			break;
 		case 'event_maubeuge':
-			picture.style.backgroundImage = 'url(../img/picture_header_eight.jpg)';
+			picture.style.backgroundImage = "url('../img/picture_header_eight.jpg')";
 			break;
 		default:
-			picture.style.backgroundImage = 'url(../img/picture_header_one.jpg)';
+			picture.style.backgroundImage = "url('../img/picture_header_one.jpg')";
 			break;
 	}
 });
